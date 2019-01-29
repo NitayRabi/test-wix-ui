30cdcae1e61652fdde97f0284b7661d83b37579f
Julius Burbulis <burbulis.julius@gmail.com>
2019-01-29 17:28:33 +0200

    add wix-style-react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'wix-style-react/Button';
+
 
 class App extends Component {
   render() {
@@ -20,6 +22,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <Button onClick={() => console.log('thanks for clicking :)')}>
+          Click me!
+        </Button>
       </div>
     );
   }
